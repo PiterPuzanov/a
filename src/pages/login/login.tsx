972e66c2e469e1b17b37cd7350bc7770ebf1797f
PiterPuzanov <peta12321@gmail.com>
2024-12-12 13:57:46 +0300

    fix

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,24 +1,33 @@
 import { FC, SyntheticEvent, useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { LoginUI } from '@ui-pages';
 import { useDispatch, useSelector } from '../../services/store';
 import { getError, login } from '../../services/slices/userSlice';
 
 export const Login: FC = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
   const localStorageEmail = localStorage.getItem('email') ?? '';
   const [email, setEmail] = useState(localStorageEmail);
   const [password, setPassword] = useState('');
   const error = useSelector(getError);
 
-  const handleSubmit = (e: SyntheticEvent) => {
+  const from = location.state?.from || '/';
+
+  const handleSubmit = async (e: SyntheticEvent) => {
     e.preventDefault();
     localStorage.setItem('email', email);
-    dispatch(
+
+    const result = await dispatch(
       login({
         email: email,
         password: password
       })
     );
+    if (result.meta.requestStatus === 'fulfilled') {
+      navigate(from);
+    }
   };
 
   return (
